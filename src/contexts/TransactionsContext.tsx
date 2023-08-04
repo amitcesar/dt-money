c6e5a1dde3c50d8b403bf90fb6f697aac59f9e88
Amit Cesar <amitcesar27@gmail.com>
2023-08-03 21:45:47 -0300

    using usememo and memo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 import { createContext } from "use-context-selector";
 import { api } from "../lib/axios";
 
@@ -33,7 +33,7 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get("transactions", {
       params: {
         _sort: "createdAt",
@@ -43,23 +43,26 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     });
 
     setTransactions(response.data);
-  }
+  }, []);
 
-  async function createTransaction(data: createTransactionInput) {
-    const { description, type, category, price } = data;
-    const response = await api.post("transactions", {
-      description,
-      type,
-      category,
-      price,
-      createdAt: new Date(),
-    });
-    setTransactions((state) => [response.data, ...state]);
-  }
+  const createTransaction = useCallback(
+    async (data: createTransactionInput) => {
+      const { description, type, category, price } = data;
+      const response = await api.post("transactions", {
+        description,
+        type,
+        category,
+        price,
+        createdAt: new Date(),
+      });
+      setTransactions((state) => [response.data, ...state]);
+    },
+    []
+  );
 
   useEffect(() => {
     fetchTransactions();
-  }, []);
+  }, [fetchTransactions]);
 
   return (
     <TransactionsContext.Provider
--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useTransaction } from "./useTransaction";
 
 
@@ -5,23 +6,26 @@ import { useTransaction } from "./useTransaction";
 export function useSummary(){
   const { transactions } = useTransaction();
 
-  const summaryReduce = transactions.reduce(
-    (summary, transactions) => {
-      if (transactions.type === "income") {
-        summary.income += transactions.price;
-        summary.total += transactions.price;
-      } else {
-        summary.outcome -= transactions.price;
-        summary.total -= transactions.price;
+  const summaryReduce = useMemo(()=> {
+    return transactions.reduce(
+      (summary, transactions) => {
+        if (transactions.type === "income") {
+          summary.income += transactions.price;
+          summary.total += transactions.price;
+        } else {
+          summary.outcome -= transactions.price;
+          summary.total -= transactions.price;
+        }
+  
+        return summary;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
       }
-
-      return summary;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+    );
+  
+  },[transactions])
   return summaryReduce;
 }
